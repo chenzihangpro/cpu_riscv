58f99f368153abe419eeb150166e6e2a0a746483
sasathreena <2117932204@qq.com>
2025-04-26 14:19:10 +0800

    modified some mistakes

--- core/if_id.v
+++ core/if_id.v
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 `include "defines.v"
 
 //在取指与译码间打一拍
@@ -44,51 +43,4 @@ module if_id(
     gen_pipe_dff #(32) int_ff(clk,rst,hold_en,`INT_NONE,int_flag_o,int_flag);
     assign int_flag_o = int_flag;
     
-=======
-`include "defines.v"
-
-//在取指与译码间打一拍
-module if_id(
-    clk,
-    rst,
-    instr_i,
-    instr_addr_i,
-    hold_flag,
-    int_flag_i,
-    int_flag_o,
-    instr_o,
-    instr_addr_o
-);
-    input wire clk;
-    input wire rst;
-    
-    input wire[`INSTR_BUS] instr_i;
-    input wire[`INSTR_ADDR_BUS] instr_addr_i;
-    
-    input wire[`HOLD_FLAG_BUS] hold_flag;
-    
-    input wire[`INT_FLAG_BUS] int_flag_i;       //中断信号
-    output wire[`INT_FLAG_BUS] int_flag_o;
-    
-    output wire[`INSTR_BUS] instr_o;
-    output wire[`INSTR_ADDR_BUS] instr_addr_o;
-    
-    wire hold_en = (hold_flag >= `HOLD_IF);
-    
-    //处理指令
-    wire[`INSTR_BUS] instr;
-    gen_pipe_dff #(32) instr_ff(clk,rst,hold_en,`INSTR_NO_OPERATION,instr_i,instr);
-    assign instr_o = instr;
-    
-    //处理指令地址
-    wire[`INSTR_ADDR_BUS] instr_addr;
-    gen_pipe_dff #(32) instr_addr_ff(clk,rst,hold_en,`ZERO_WORD,instr_addr_i,instr_addr);
-    assign instr_addr_o = instr_addr;
-    
-    //处理中断信号
-    wire[`INT_FLAG_BUS] int_flag;
-    gen_pipe_dff #(32) int_ff(clk,rst,hold_en,`INT_NONE,int_flag_o,int_flag);
-    assign int_flag_o = int_flag;
-    
->>>>>>> 20def4fb511b0f8e452290cc4a3546b8042b4324
 endmodule
